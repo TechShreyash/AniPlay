dcd6ea12b070b02f4ec4eeaaaae61137dea4ee04
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-11-26 19:11:25 +0530

    Report Cmd Added

--- AniPlay/plugins/cmd.py
+++ AniPlay/plugins/cmd.py
@@ -5,30 +5,42 @@ from AniPlay.plugins.AnimeDex import AnimeDex
 from AniPlay.plugins.button import BTN
 
 
-@app.on_message(filters.command(['start', 'ping', 'help', 'alive']))
+@app.on_message(filters.command(["start", "ping", "help", "alive"]))
 async def start(_, message: Message):
     try:
-        await message.reply_text('Bot Is Online...\n\nSearch Animes Using /search or /s')
+        await message.reply_text(
+            "Bot Is Online...\n\nSearch Animes Using /search or /s"
+        )
     except:
         return
 
 
-QUERY = '**Search Results:** `{}`'
+QUERY = "**Search Results:** `{}`"
 
 
-@app.on_message(filters.command(['search', 's']))
+@app.on_message(filters.command(["search", "s"]))
 async def searchCMD(_, message: Message):
     try:
         user = message.from_user.id
-        query = ' '.join(message.command[1:])
-        if query == '':
-            return await message.reply_text('Give me something to search ^_^')
+        query = " ".join(message.command[1:])
+        if query == "":
+            return await message.reply_text("Give me something to search ^_^")
         data = AnimeDex.search(query)
         button = BTN.searchCMD(user, data, query)
-        await message.reply_text(f'{QUERY.format(query)}\n\n© {message.from_user.mention}', reply_markup=button)
+        await message.reply_text(
+            f"{QUERY.format(query)}\n\n© {message.from_user.mention}",
+            reply_markup=button,
+        )
     except Exception as e:
         print(e)
         try:
-            return await message.reply_text('**Anime Not Found...**\n\nProbably Incorrect Name, Try again')
+            return await message.reply_text(
+                "**Anime Not Found...**\n\nProbably Incorrect Name, Try again"
+            )
         except:
             return
+
+
+@app.on_message(filters.command(["report"]))
+async def reportCMD(_, message: Message):
+    await message.reply_text("Report Bugs Here: @TechZBots_Support")
